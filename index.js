9b841e8d4f6c017e4a3dda4811f27fb794bdb321
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 20:42:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function prependKitten(name) {
 
 function removeLastKitten() {
   var kittens = ["Milo", "Otis", "Garfield"]
-  kittens.slice(-1)
+  kittens.slice(0, kittens.length - 1)
   return kittens;
 }
