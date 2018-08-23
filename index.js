b9ee3d2022a50dbe94d27f76c328b71250ae82a1
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 20:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions and code here
+const kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob");
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(2);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(0);
+  return kittens;
+}
+
+function appendKitten(name) {
+  var broom = ["Broom"];
+  return kittens.concat(broom);
+}
+
+function prependKitten(name) {
+  var arnold = ["Arnold"];
+  return arnold.concat(kittens);
+}
+
+function removeLastKitten() {
+  kittens.slice(2, 2)
+  return kittens;
+}
